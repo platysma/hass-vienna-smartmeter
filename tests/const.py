af13ca2bfda64cfe3318d76bb59faf4b1fbe88c5
Platysma <platysma.dev@gmail.com>
2021-10-04 17:00:15 +0200

    Move MOCK_CONFIG to const.py

---
+++ tests/const.py
@@ -0,0 +1,7 @@
+"""Constants for smartmeter integration tests."""
+from custom_components.vienna_smartmeter import const
+
+MOCK_CONFIG = {
+    const.CONF_USERNAME: const.DEFAULT_USERNAME,
+    const.CONF_PASSWORD: const.DEFAULT_PASSWORD,
+}
